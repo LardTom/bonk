251bee6b457abda2a2b98aa9df3ce563d2553d02
LardTom <44534151+LardTom@users.noreply.github.com>
2021-03-16 20:52:10 +0100

    update

--- styles/script.js
+++ styles/script.js
@@ -1,15 +1,25 @@
-const audio = new Audio('../media/bonkmeme.mp3');
+const audio = new Audio('media/bonkmeme.mp3');
 let count = 0;
+let isbonk = false;
 function playbonk(){
-    document.getElementById("before").classList.add("hidden");
-    document.getElementById("after").classList.remove("hidden");
-    audio.volume = 0.1;
-    audio.play();
-    count ++;
-    document.getElementById("count").innerHTML = `Your Bonkcount: ${count}`;
+    
+
+    if(!isbonk) {
+        document.getElementById("before").classList.add("hidden");
+        document.getElementById("after").classList.remove("hidden");
+        audio.volume = 0.1;
+        audio.play();
+        count ++;
+        document.getElementById("count").innerHTML = `Your Bonkcount: ${count}`;
+        isbonk = true;
+        setTimeout(() => {
+            reset();
+            isbonk = false;
+        }, 500);
+    }
 }
 
-function reset(){
+function reset() {
     document.getElementById("before").classList.remove("hidden");
     document.getElementById("after").classList.add("hidden");
 }
